3a38fb6eafa5b83a233d7a77cf86643becacc168
mohammad.juned <mohammad.juned@aurea.com>
2017-04-28 09:01:07 +0530

    Review : last week file changed(Author C Marcelo) by Author A(mjuned)

--- src/main/java/com/devfactory/scminsights/Constants.java
+++ src/main/java/com/devfactory/scminsights/Constants.java
@@ -10,36 +10,41 @@ package com.devfactory.scminsights;
  */
 public interface Constants {
 
-	/**
-	 * Constants for endpoint.
-	 */
-	public static final String ENDPOINT = "http://localhost:9090";
+    /**
+     * Constants for endpoint.
+     */
+    public static final String ENDPOINT = "http://localhost:9090/api_v2";
 
-	/**
-	 * Constants for collect endpoint.
-	 */
-	public static final String COLLECT_ENDPOINT = ENDPOINT + "/collect";
+    /**
+     * Constants for collect endpoint.
+     */
+    public static final String COLLECT_ENDPOINT = ENDPOINT + "/api_v2/collect";
 
-	/**
-	 * Constants for query endpoint.
-	 */
-	public static final String QUERY_ENDPOINT = ENDPOINT + "/query";
+    /**
+     * Constants for query endpoint.
+     */
+    public static final String QUERY_ENDPOINT = ENDPOINT + "/api_v2/query";
 
-	/**
-	 * Constants for Collect airports. params as /airport/{iata}/{lat}/{long}
-	 */
-	public static final String AIRPORT_COLLECT_ENDPOINT = "/airport/%s/%s/%s";
+    /**
+     * Constants for Collect airports. params as /airport/{iata}/{lat}/{long}
+     */
+    public static final String AIRPORT_COLLECT_ENDPOINT = "/api_v2/airport/%s/%s/%s";
 
-	/** earth radius in KM */
-	public static final double R = 6372.8;
+    /** earth radius in KM */
+    public static final double R = 6372.8;
 
-	/**
-	 * airport file word separator.
-	 */
-	public static final String WORD_SEPARATOR = ",";
+    /**
+     * airport file word separator.
+     */
+    public static final String WORD_SEPARATOR = ",";
 
-	/**
-	 * Total field length in dat file.
-	 */
-	public static final short TOTAL_FIELD_LENGTH = 11;
+    /**
+     * Total field length in dat file.
+     */
+    public static final short TOTAL_FIELD_LENGTH = 11;
+
+    /**
+     * Constants for Collect users.
+     */
+    public static final String USER_COLLECTOR = "/api_v2/user";
 }
