4a81209589aa441353889e4f961b8214ad87196f
mohammad.juned <mohammad.juned@aurea.com>
2017-04-28 08:57:26 +0530

    Review 30 days age file changed(Author B Raul) by Author A(mjuned)

--- src/main/java/com/devfactory/scminsights/Message.java
+++ src/main/java/com/devfactory/scminsights/Message.java
@@ -11,11 +11,13 @@ public class Message {
 
     String NAME_NOT_NULL = "Search name can't be empty..";
 
-    String NO_CLASS_FOUND = "No Class found for the given name: %s";
+    String NO_CLASS_FOUND = "Class not found for the given name: %s";
 
-    String NO_PROJECT_FOUND = "NO project found for provided id: %s";
+    String NO_PROJECT_FOUND = "Project not found for provided id: %s";
 
-    String NO_PACKAGE_FOUND = "NO package found for provided id: %s and name: %s";
+    String NO_PACKAGE_FOUND = "Package not found for provided id: %s and name: %s";
+    
+    String NO_COMMIT_FOUND = "Commit not found for provided id: %s and name: %s";
   }
   
   public void update(){
